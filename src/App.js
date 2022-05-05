d8c30c111c1b3bc3b03bbd2c7e18fdd5d9393470
mosheurpau <mosheur.pau@gamil.com>
2022-05-06 02:00:30 +0600

    added all router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AddItem from "./Pages/AddItem/AddItem";
+import Blogs from "./Pages/Blogs/Blogs";
+import Home from "./Pages/Home/Home/Home";
+import Items from "./Pages/Home/Items/Items";
+import ManageItems from "./Pages/ManageItems/ManageItems";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
+import NotFound from "./Pages/Shared/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <h2>This is furniture warehouse shop.</h2>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/items" element={<Items></Items>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/manage" element={<ManageItems></ManageItems>}></Route>
+        <Route path="/addItem" element={<AddItem></AddItem>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
