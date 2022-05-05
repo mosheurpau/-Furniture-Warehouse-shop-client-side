f1b5f6f8571a218d6b8725c762b9f462b43dbfa8
mosheurpau <mosheur.pau@gamil.com>
2022-05-06 03:58:13 +0600

    added header and customlink

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,48 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CustomLink from "../../CustomLink/CustomLink";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h2>This is Header</h2>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            alt=""
+            src="https://i.ibb.co/K7hH6Tb/furniture-logo.png"
+            width="70"
+            height="70"
+            className="d-inline-block align-center nav-logo"
+          />{" "}
+          <b className="logo-title">
+            <span className="text-warning">Furniture</span> Warehouse
+          </b>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="text-center">
+          <Nav className="ms-auto">
+            <Nav.Link as={CustomLink} to="/home"></Nav.Link>
+            <Nav.Link as={CustomLink} to="/items">
+              Inventory Item
+            </Nav.Link>
+            <Nav.Link as={CustomLink} to="/addItem">
+              Add Items
+            </Nav.Link>
+            <Nav.Link as={CustomLink} to="/manage">
+              Manage Item
+            </Nav.Link>
+            <Nav.Link as={CustomLink} to="/blogs">
+              Blogs
+            </Nav.Link>
+            <Nav.Link as={CustomLink} to="/login">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
