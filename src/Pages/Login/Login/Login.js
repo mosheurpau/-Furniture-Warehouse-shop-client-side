b659fc18c295fe74b7fb40535ea6844a991a3543
mosheurpau <mosheur.pau@gamil.com>
2022-05-07 11:31:51 +0600

    added register and loading navigate and toast

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,30 +1,119 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { async } from "@firebase/util";
 
-const login = () => {
+const Login = () => {
+  const emailRef = useRef("");
+  let errorElement;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const [sendPasswordResetEmail] = useSendPasswordResetEmail(auth);
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  let from = location.state?.from?.pathname || "/";
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
+  if (error) {
+    errorElement = <p className="text-danger">Error: {error?.message}</p>;
+  }
+
+  const navigateRegister = () => {
+    navigate("/register");
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    signInWithEmailAndPassword(email, password);
+    console.log(email, password);
+  };
+
+  const resetPassword = async () => {
+    const email = emailRef.current.value;
+    if (email) {
+      await sendPasswordResetEmail(email);
+      toast("Sent email");
+    } else {
+      toast("Please enter your email address");
+    }
+  };
+
   return (
     <Container>
       <Row className="justify-content-md-center">
-        <Col xs="10" lg="6" className="mt-5 mx-auto">
+        <Col xs="10" lg="5" className="mt-3 mx-auto bg-light rounded-top px-5">
           <h2 className="text-center my-3">Please Login</h2>
-          <Form>
+          <Form onSubmit={handleLogin} className="mb-3">
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" required />
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                name="email"
+                placeholder="Enter email"
+                required
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" required />
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
             </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
+            <Button variant="primary" type="submit" className="w-100">
+              Login
             </Button>
           </Form>
+          {errorElement}
+          <p>
+            New to Furniture warehouse?{" "}
+            <Link
+              to="/register"
+              className="text-primary text-decoration-none"
+              onClick={navigateRegister}
+            >
+              Please Register
+            </Link>
+          </p>
+          <p>
+            Forget Password?{" "}
+            <span
+              style={{ cursor: "pointer" }}
+              className="text-primary"
+              onClick={resetPassword}
+            >
+              Reset Password
+            </span>
+          </p>
+          <ToastContainer />
         </Col>
       </Row>
     </Container>
   );
 };
 
-export default login;
+export default Login;
